69c8701ae4d113ac7b233afcf1222dcb2af40588
ddthang86 <ddthang86@gmail.com>
2023-11-30 15:44:00 +0700

    update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setuptools
+
+setup(
+    name = "sensor",
+    version = "0.0.1",
+    author = "ThangDT",
+    author_email = "ddthang86@gmail.com",
+    packages = find_packages(),
+
+) 
